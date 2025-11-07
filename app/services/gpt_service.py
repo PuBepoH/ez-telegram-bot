1878dc77d4229c7b2742a01c5e8fef49acde269c
pubepoh <pubepoh@gmail.com>
2025-11-07 22:10:54 +0300

    fixed temperature for 1.0 for gpt5

--- app/services/gpt_service.py
+++ app/services/gpt_service.py
@@ -12,7 +12,7 @@ def ask_gpt(user_text_and_context: list[dict[str, str]]) -> str:
     response = openai_client.chat.completions.create(
         model=settings.openai_model,
         messages=user_text_and_context,
-        temperature=0.7,
+        temperature=1,
     )
 
     raw_answer = response.choices[0].message.content
